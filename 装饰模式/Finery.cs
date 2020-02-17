5f163c0a91411a661ae0e0c41555e52232fe1e7e
CHENYUAN <chenyuan@hicore.com>
2020-02-17 15:42:22 +0800

    feta: 装饰模式

---
+++ 装饰模式/Finery.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 装饰模式
+{
+    abstract class Finery
+    {
+        public abstract void Show();
+    }
+    class TShirts : Finery
+    {
+        /// <summary>
+        /// 大T恤
+        /// </summary>
+        public override void Show()
+        {
+            Console.Write("大 T 恤 ");
+        }
+    }
+
+    class BigTrouser : Finery
+    {
+        /// <summary>
+        /// 垮裤
+        /// </summary>
+        public override void Show()
+        {
+            Console.Write("垮裤 ");
+        }
+    }
+
+    class Sneakers : Finery
+    {
+        /// <summary>
+        /// 破球鞋
+        /// </summary>
+        public override void Show()
+        {
+            Console.Write("破球鞋 ");
+        }
+    }
+
+    class Suit : Finery
+    {
+        /// <summary>
+        /// 西装
+        /// </summary>
+        public override void Show()
+        {
+            Console.Write("西装 ");
+        }
+    }
+
+    class Tie : Finery
+    {
+        /// <summary>
+        /// 领带
+        /// </summary>
+        public override void Show()
+        {
+            Console.Write("领带 ");
+        }
+    }
+
+    class LeatherShoes : Finery
+    {
+        /// <summary>
+        /// 皮鞋
+        /// </summary>
+        public override void Show()
+        {
+            Console.Write("皮鞋 ");
+        }
+    }
+}
---
+++ 装饰模式/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 装饰模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("陈远");
+            Console.WriteLine("\n 第一种装扮：");
+            TShirts shirts = new TShirts();
+            BigTrouser bigTrouser = new BigTrouser();
+            Sneakers sneakers = new Sneakers();
+
+            shirts.Show();
+            bigTrouser.Show();
+            sneakers.Show();
+            person.Show();
+            Console.ReadLine();
+        }
+    }
+}
